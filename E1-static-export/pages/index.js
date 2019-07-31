8f9ba8018a83e54630615a9542ccd1cfd1fdd13e
Luis Fernando Alvarez D <luis@zeit.co>
2019-07-31 13:06:46 -0500

    Update the static-export example (#15)

--- E1-static-export/pages/index.js
+++ E1-static-export/pages/index.js
@@ -1,25 +1,31 @@
-import Layout from '../components/MyLayout.js'
+import Layout from '../components/MyLayout'
 import Link from 'next/link'
+import fetch from 'isomorphic-unfetch'
 
-function PostLink(props) {
-  return (
-    <li>
-      <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
-        <a>{props.title}</a>
-      </Link>
-    </li>
-  )
+const Index = props => (
+  <Layout>
+    <h1>Batman TV Shows</h1>
+    <ul>
+      {props.shows.map(show => (
+        <li key={show.id}>
+          <Link href="/show/[id]" as={`/show/${show.id}`}>
+            <a>{show.name}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </Layout>
+)
+
+Index.getInitialProps = async function() {
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman')
+  const data = await res.json()
+
+  console.log(`Show data fetched. Count: ${data.length}`)
+
+  return {
+    shows: data.map(entry => entry.show)
+  }
 }
 
-export default function Index() {
-  return (
-    <Layout>
-      <h1>My Blog</h1>
-      <ul>
-        <PostLink id="hello-nextjs" title="Hello Next.js" />
-        <PostLink id="learn-nextjs" title="Learn Next.js is awesome" />
-        <PostLink id="deploy-nextjs" title="Deploy apps with Zeit" />
-      </ul>
-    </Layout>
-  )
-}
+export default Index
