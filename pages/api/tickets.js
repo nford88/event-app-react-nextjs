6204d2f429b0e17a59cb6725424fb832897ecc63
Nick Ford <nick.ford@visma.com>
2021-02-04 21:28:41 +0000

    Integrate cache on api, env tokens,

--- pages/api/tickets.js
+++ pages/api/tickets.js
@@ -3,14 +3,32 @@ const fetch = require('node-fetch')
 const isBefore = require('date-fns/isBefore')
 import { groupBy, chain } from 'lodash'
 import { dateRangesForTicketMasterApi } from '../../components/utils'
-// import data from './events.json'
+import localData from './events.json'
 
-let dataS = []
+export let cachedData = [];
 
-const fetchApi = () => {
-  const API_URL = 'https://app.ticketmaster.com/discovery/v2/events.json?'
+const handler = async (req, res) => {
+  let modifiedData = [];
+  console.warn(process.env.LOCAL_DATA);
+  if (process.env.LOCAL_DATA == 'true'){
+    const { events } = localData
+    cachedData =  events ;
+    modifiedData = formatEventResults(events);
+  }
+  else {
+    const { _embedded } = await fetchEventData()
+    cachedData = { ..._embedded.events };
+    const apiData = _embedded.events.filter(event => {
+      return isBefore(new Date(), new Date(event.dates.start.dateTime))
+    })
+    modifiedData = formatEventResults(apiData);
+  }
+  return res.status(200).json(modifiedData)
+}
+
+const fetchEventData = () => {
   const params = {
-    apikey: 'bQ8tC2OMgdsB5mWdbNGEGR3d1V1bTc5v',
+    apikey: process.env.TM_API_TOKEN,
     sort: 'date,name,asc',
     size: '200',
     geoPoint: 'gc7x7q',
@@ -19,36 +37,18 @@ const fetchApi = () => {
     localStartDateTime: dateRangesForTicketMasterApi()
   }
   let queryString = querystring.stringify(params)
-  if (dataS.length === 0) {
-    dataS = fetch(API_URL + queryString).then(r => r.json())
-    // console.log(dataS)
-    return dataS
-  } else {
-    return dataS
+  if (cachedData.length === 0) {
+    cachedData = fetch('https://app.ticketmaster.com/discovery/v2/events.json?' + queryString).then(r => r.json())
   }
+  return cachedData;
 }
 
-const formatEventResults = data => {
-  const filteredEvents = data['_embedded'].events.filter(event => {
-    return isBefore(new Date(), new Date(event.dates.start.dateTime))
-  })
-
-  const sorted = chain(filteredEvents)
+const formatEventResults = (events) => {
+  const sorted = chain(events)
     .groupBy(result => result.dates.start.localDate)
     .map((events, key) => ({ date: key, events: events }))
     .value()
-
-  // sorted.map(d => {
-  //   return console.log('day =', d)
-  // })
-
   return sorted
 }
 
-const handler = async (req, res) => {
-  const data = await fetchApi()
-  const modifiedData = await formatEventResults(data)
-  // const modifiedData = data
-  return res.status(200).json(modifiedData)
-}
-export default handler
+export default handler;
