51f0d127a4c24e3585c0f641f11ee98de5e6ff7e
nford.work@gmail.com <nf@nf.com>
2021-02-15 21:28:26 +0000

    PWA implementation

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './global_css.css'
+import { useEffect } from "react"
 
 function MyApp({ Component, pageProps }) {
+
+  useEffect(() => {
+    if("serviceWorker" in navigator) {
+      window.addEventListener("", function () {
+       navigator.serviceWorker.register("/sw.js").then(
+          function (registration) {
+            console.log("Service Worker registration successful with scope: ", registration.scope);
+          },
+          function (err) {
+            console.log("Service Worker registration failed: ", err);
+          }
+        );
+      });
+    }
+  }, [])
+
   return <Component {...pageProps} />
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,19 @@
 import EventsNav from '../components/eventsNav'
 import EventsMain from './events'
 import { Jumbotron, Container } from 'reactstrap'
+import Head from 'next/head';
 
 export default function Index() {
   return (
     <div>
+       <Head>
+        <title>Dublin Event and Arts Guide</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#7392b7" />
+        <link rel="apple-touch-icon" href="/logo-96x96.png" />
+        <meta name="apple-mobile-web-app-status-bar" content="#7392b7" />
+      </Head>
       <EventsNav />
       <Jumbotron className="events-home-jumbotron">
         <Container>
